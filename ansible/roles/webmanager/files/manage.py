598a15eda56622944f7ecfc00876f2d016f4f3a1
auraltension <auraltension@riseup.net>
2015-01-14 00:28:05 +1100

    Function to write hostapd config file

--- ansible/roles/webmanager/files/manage.py
+++ ansible/roles/webmanager/files/manage.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 import re
 import subprocess
+import textwrap
 from bottle import get, post, request, run, route, template
 
 usb_count = 2
 interfaces_file = '/etc/network/interfaces'
+
+def hostapd_config(iface, ssid, psk, card='edimax', hide=False):
+    f = '/etc/hostapd/hostapd.conf'
+    if card == 'edimax':
+        driver = 'rtl871xdrv'
+    else:
+        driver = 'nl80211'
+    config = textwrap.dedent("""\
+        interface=%s
+	ssid=%s
+	wpa_passphrase=%s
+	driver=%s
+	hw_mode=g
+	channel=6
+	macaddr_acl=0
+	auth_algs=1
+	ignore_broadcast_ssid=0
+	wpa=2
+	wpa_key_mgmt=WPA-PSK
+	wpa_pairwise=TKIP
+	rsn_pairwise=CCMP
+        """ % (iface, ssid, psk, driver))
+    with open(f, "w") as file:
+        file.write(config)
 
 def set_wifi_client(ssid, psk, iface='wlan0'):
     wpa_ssid = '    wpa-ssid ' + ssid
@@ -27,6 +52,8 @@ def set_wifi_client(ssid, psk, iface='wlan0'):
 def set_wifi_ap(ssid, psk, iface='wlan1'):
     interfaces_file = '/etc/network/interfaces'
     subprocess.call(["ifdown", iface])
+
+    #TODO set ifaces file:
     with open(interfaces_file, "r") as interfaces:
         lines = interfaces.readlines()
     with open(interfaces_file, "w") as interfaces:
@@ -37,7 +64,12 @@ def set_wifi_ap(ssid, psk, iface='wlan1'):
     with open(interfaces_file, "w") as interfaces:
         for line in lines:
             interfaces.write(re.sub(r'^    wpa-psk.*', wpa_psk, line))
+
+
+    hostapd_config(iface, ssid, psk, edimax, hide=False)
+    service('hostapd', 'restart')
     subprocess.call(["ifup", iface])
+
     return True
 
 #def config_interface(iface, mode, data, enable=True):
